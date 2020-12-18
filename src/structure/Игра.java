d24eb2480f7cb28b4f6acc5a0a3d0f16b3c0c105
Kopin Hruk <kopinhruk@gmail.com>
2020-12-18 17:26:45 +0300

    first version

---
+++ src/structure/Игра.java
@@ -0,0 +1,41 @@
+package structure;
+
+public class Игра {
+    public Таймер таймер;
+
+    public Счет счет_команды_1;
+    public Счет счет_команды_2;
+
+    public Команда команда1;
+    public Команда команда2;
+
+    public Статистика статистика;
+
+    public Перерыв перерыв;
+
+    public Настройка настройки;
+
+
+
+
+    public void запуск(){
+    };
+
+    public void конец(){
+    };
+
+    public void перерыв(){
+    };
+
+    public void запсук_таймера(){
+    };
+
+    public void увеличить_счет_первой_команды(){
+    };
+
+    public void увеличить_счет_второй_команды(){
+    };
+
+    public void сохранить(){
+    };
+}
